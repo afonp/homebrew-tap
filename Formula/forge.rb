975ba2f1270b51d9decfdb0a4c054dbc8b1aeb2d
Afonso Pereira <afonso@afpereira.me>
2026-02-26 00:39:05 +0000

    Add Forge formula for competitive programming

---
+++ Formula/forge.rb
@@ -0,0 +1,31 @@
+class Forge < Formula
+  desc "competitive programming exercise scaffolder with a c++ template system"
+  homepage "https://github.com/afonp/forge"
+  license "MIT"
+
+  on_macos do
+    on_arm do
+      url "https://github.com/afonp/forge/releases/download/v0.1.1/forge-aarch64-apple-darwin.tar.gz"
+    end
+    on_intel do
+      url "https://github.com/afonp/forge/releases/download/v0.1.1/forge-x86_64-apple-darwin.tar.gz"
+    end
+  end
+
+  on_linux do
+    on_arm do
+      url "https://github.com/afonp/forge/releases/download/v0.1.1/forge-aarch64-unknown-linux-gnu.tar.gz"
+    end
+    on_intel do
+      url "https://github.com/afonp/forge/releases/download/v0.1.1/forge-x86_64-unknown-linux-gnu.tar.gz"
+    end
+  end
+
+  def install
+    bin.install "forge"
+  end
+
+  test do
+    assert_match "competitive programming exercise scaffolder", shell_output("#{bin}/forge --help")
+  end
+end
